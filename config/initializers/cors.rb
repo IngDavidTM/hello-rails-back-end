8f503c364a860afd795eae9d6ad9a26b197c999d
IngDavidTM <david-1011@hotmail.com>
2023-02-24 19:44:00 -0500

    Add required changes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5100'
-    resource '*', headers: :any, methods: [:get, :post, :options]
+    origins "*"
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
